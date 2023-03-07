b62a1cd601b1c56dcc84092af41314b1a6f362b0
andreas <podovinov2003andrey@gmail.com>
2023-03-07 23:04:03 +0600

    fix users model

--- server/src/models/schemas/users/users_response.py
+++ server/src/models/schemas/users/users_response.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, UUID4
+from datetime import datetime
 
 
 class UsersResponse(BaseModel):
     guid: UUID4
     login: str
+    created_at: datetime
 
     class Config:
         orm_mode = True
--- server/src/models/users.py
+++ server/src/models/users.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.sql import func
 from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
 from src.models.base import Base
 
@@ -8,3 +9,4 @@ class Users(Base):
     guid = Column(GUID, primary_key=True, default=GUID_DEFAULT_SQLITE)
     login = Column(String, nullable=False, unique=True)
     password_hashed = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
