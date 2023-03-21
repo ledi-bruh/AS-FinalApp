3b6d4eefc66ef404445ef79c36ace3eee78bcf1c
andreas <podovinov2003andrey@gmail.com>
2023-03-21 15:30:54 +0600

    interval check auth

--- client/src/pages/auth/auth.py
+++ client/src/pages/auth/auth.py
@@ -7,7 +7,14 @@ layout = dbc.Container(
     [
         dcc.Input(id='username-input', type='text', placeholder='Username'),
         dcc.Input(id='password-input', type='password', placeholder='Password'),
-        html.Button('Sign In', id='sign-in-button'),
+        html.Button('Sign in', id='sign-in-button'),
+        html.Button('Sign out', id='sign-out-button'),
         html.Div(id='auth-output'),
+        html.Div(id='auth-status-output'),
+        dcc.Interval(
+            id='auth-status-interval',
+            interval=1000,
+            n_intervals=0
+        )
     ]
 )
--- client/src/pages/auth/auth_callbacks.py
+++ client/src/pages/auth/auth_callbacks.py
@@ -1,5 +1,5 @@
 import requests
-from dash import Input, Output, State
+from dash import Input, Output, State, callback_context, no_update
 from dash.exceptions import PreventUpdate
 from app import app
 from src.core.settings import settings
@@ -23,16 +23,36 @@ def authenticate_user(username: str, password: str):
 
 
 @app.callback([Output('auth-output', 'children'),
-               Output('token-store', 'data')],
-              [Input('sign-in-button', 'n_clicks')],
+               Output('token-store', 'data'),],
+              [Input('sign-in-button', 'n_clicks'),
+               Input('sign-out-button', 'n_clicks')],
               [State('username-input', 'value'),
-               State('password-input', 'value')],
+               State('password-input', 'value'),
+               State('token-store', 'data'),],
               prevent_initial_call=True)
-def authenticate_user_callback(n_clicks, username: str, password: str):
-    if n_clicks is None:
+def authenticate_user_callback(clicks1, clicks2, username, password, token):
+    if clicks1 is None:
         raise PreventUpdate
-    token = authenticate_user(username, password)
-    if token:
-        return 'Successful', token
+    
+    ctx = callback_context
+    if not ctx.triggered:
+        return no_update
+    
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    
+    if button_id == 'sign-out-button':
+        return 'Вы вышли из аккаунта', None
+    
+    if (token := authenticate_user(username, password)):
+        return 'Вы вошли в аккаунт', token
     else:
         return 'Authentication error', None
+
+
+@app.callback(Output('auth-status-output', 'children'),
+              Input('auth-status-interval', 'n_intervals'),
+              State('token-store', 'data'),)
+def authenticate_status_callback(n,  token):
+    if token is None:
+        return '❌'
+    return '✅'
